bcbe9d43193cee03dd63f22928fe6c2c3bd1aacf
goyanipriya3-dot <goyanipriya3@gmail.com>
2026-03-11 03:45:16 +0000

    ArrayDemo.c

---
+++ ArrayDemo.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/*
+Array : It is a collection of data of similar data
+*/
+int main() {
+    //Student matks of 5 Subjegs
+    //int a(5)={55,45,47,67,74},total=0;
+   // for(int i=0;i<=5;i++){
+     //   //subject 1: marks 55
+    //    printf("\n subject %d : marks %d \n",i+1,a(i))
+    //    total=total+a(i);
+   //  }
+    // printf("\n total %d",total); 
+
+    //int temp(7)={23,34,33,40,38,39,41};
+
+    //for(int i=0;i<=7;i++){
+        //Day 1 : Temp 23
+    //    printf("\n Day %d : Temp %d \n",i+1,temp(i));
+  //  }
+
+     //score (100,123,178,230)
+    //Player 1 : Score 100
+     
+    int score=(100,123,178,230)
+
+    for(int i=0,i<=4,i++);{
+       printf("\n Player %d: score %d \n", i+1, score(i));
+    }
+    return 0;
+}
