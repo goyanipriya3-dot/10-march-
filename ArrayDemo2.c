0740c62ac5f5377ec0b482243f7c1ce9f22d8a03
goyanipriya3-dot <goyanipriya3@gmail.com>
2026-03-11 09:29:19 +0530

    Correct syntax and score display in ArrayDemo2.c
    Fix syntax errors and improve score handling.

---
+++ ArrayDemo2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+     //score (100,123,178,230)
+    //Player 1 : Score 100
+     
+    int score=(100,123,178,230)
+
+    for(int i=0,i<=4,i++);{
+       printf("\n Player %d: score %d \n", i+1, score(i));
+    }
+    return 0;
+}
